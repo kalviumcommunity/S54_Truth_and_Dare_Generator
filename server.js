68e2c43bea01ac71e8a9c21267e28cb027978d0f
phoenix1531 <arjun15310@gmail.com>
2024-02-01 15:43:17 +0530

    module 3

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/ping", (req, res) => res.send("pong"));
+
+app.listen(3000, () => {
+  console.log(`🚀 server running on PORT: 3000`);
+});
