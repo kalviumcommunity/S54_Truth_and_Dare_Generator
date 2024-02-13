d1ea6f8388cde8f12a87a90efd7b172d65b7dda8
phoenix1531 <arjun15310@gmail.com>
2024-02-13 10:34:24 +0530

    clientside began

---
+++ Client/src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div style={{height:"150vh", textAlign:"center"}}>
+      <h1> Welcome to  the home page! 😎</h1>   
+   </div>
+  )
+}
+
+export default Home
